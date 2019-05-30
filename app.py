dc516bb3da0a98a47918c95f950455b06d40c3b9
u6k <u6k.apps@gmail.com>
2019-05-30 17:53:57 +0900

    Added: top companies

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import investment_stocks_predict_trend as app
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("subcommand", help="processing_by_company")
+    parser.add_argument("subcommand", help="processing_by_company, top_companies")
 
     args = parser.parse_args()
 
     if args.subcommand == "processing_by_company":
         app.processing_by_company()
+    elif args.subcommand == "top_companies":
+        app.top_companies()
     else:
         raise Exception("unknown subcommand: " + args.subcommand)
 
--- investment_stocks_predict_trend/__init__.py
+++ investment_stocks_predict_trend/__init__.py
@@ -15,22 +15,22 @@ def processing_by_company():
     print(df_companies_csv.head())
     print(df_companies_csv.tail())
 
-    df_result = df_companies_csv.copy()
-    df_result = df_result[["ticker_symbol", "name"]]
-    df_result = df_result.dropna()
-    df_result = df_result.assign(ticker_symbol=df_result["ticker_symbol"].astype(int))
-    df_result = df_result.sort_values("ticker_symbol")
-    df_result = df_result.drop_duplicates()
-    df_result = df_result.set_index("ticker_symbol")
-    df_result = df_result.assign(profit=0.0)
-    df_result = df_result.assign(volume=0.0)
-    df_result = df_result.assign(data_size=0.0)
+    df_profit = df_companies_csv.copy()
+    df_profit = df_profit[["ticker_symbol", "name"]]
+    df_profit = df_profit.dropna()
+    df_profit = df_profit.assign(ticker_symbol=df_profit["ticker_symbol"].astype(int))
+    df_profit = df_profit.sort_values("ticker_symbol")
+    df_profit = df_profit.drop_duplicates()
+    df_profit = df_profit.set_index("ticker_symbol")
+    df_profit = df_profit.assign(profit=0.0)
+    df_profit = df_profit.assign(volume=0.0)
+    df_profit = df_profit.assign(data_size=0.0)
 
-    df_result.info()
-    print(df_result.head())
-    print(df_result.tail())
+    df_profit.info()
+    print(df_profit.head())
+    print(df_profit.tail())
 
-    for symbol in df_result.index:
+    for symbol in df_profit.index:
         df_prices_csv = pd.read_csv("local/stock_prices." + str(symbol) + ".csv")
 
         df = df_prices_csv.copy()
@@ -46,16 +46,32 @@ def processing_by_company():
             else:
                 df.at[idx, "profit"] = 0.0
 
-        df_result.at[symbol, "data_size"] = len(df)
+        df_profit.at[symbol, "data_size"] = len(df)
 
         if len(df) > 250:
             df_subset = df[-250:].copy()
-            df_result.at[symbol, "profit"] = df_subset["profit"].sum()
-            df_result.at[symbol, "volume"] = df_subset["turnover"].sum()
+            df_profit.at[symbol, "profit"] = df_subset["profit"].sum()
+            df_profit.at[symbol, "volume"] = df_subset["turnover"].sum()
 
-        print(df_result.loc[symbol])
+        print(df_profit.loc[symbol])
 
-    df_result.to_csv("local/result.csv")
+    df_profit.to_csv("local/profit.csv")
 
-    return df_result
+    return df_profit
+
+def top_companies():
+    df_profit = pd.read_csv("local/profit.csv", index_col=0)
+    df_profit.info()
+    print(df_profit.head())
+    print(df_profit.tail())
+
+    df = df_profit.copy()
+    df = df.query("data_size > 2500.0")
+    df = df.query("volume > 10000000")
+    df = df.query("profit > 5000.0")
+    print(df)
+
+    df.to_csv("local/top_companies.csv")
+
+    return df
 
