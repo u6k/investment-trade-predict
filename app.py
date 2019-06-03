0fb394bf52a0d4f0a8c5674d42518d68218e19a3
u6k <u6k.apps@gmail.com>
2019-06-03 20:16:43 +0900

    Changed: agent 1 script

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import argparse
 from investment_stocks_predict_trend import select_company
 from investment_stocks_predict_trend import random_forest_1
 from investment_stocks_predict_trend import random_forest_2
+from investment_stocks_predict_trend import agent_1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -18,5 +19,8 @@ if __name__ == "__main__":
         random_forest_1.scores()
     elif args.subcommand == "random_forest_2.scores":
         random_forest_2.scores()
+    elif args.subcommand == "agent_1":
+        # FIXME
+        df = agent_1.preprocessing()
     else:
         raise Exception("unknown subcommand: " + args.subcommand)
