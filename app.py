9f563aebac96bb47a1ba62209f27c3d9b7283726
u6k <u6k.apps@gmail.com>
2019-06-04 13:22:35 +0900

    Changed: learn 3 to agent 2

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import argparse
 import os
 from comet_ml import Experiment
-
 from investment_stocks_predict_trend import select_company
 from investment_stocks_predict_trend import random_forest_1
 from investment_stocks_predict_trend import random_forest_2
 from investment_stocks_predict_trend import agent_1
+from investment_stocks_predict_trend import agent_2
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -25,6 +26,11 @@ if __name__ == "__main__":
         experiment = Experiment(api_key=os.environ["COMET_ML_API_KEY"],
                                 project_name="agent_1")
         agent_1.execute(experiment)
+        experiment.end()
+    elif args.subcommand == "agent_2":
+        experiment = Experiment(api_key=os.environ["COMET_ML_API_KEY"],
+                                project_name="agent_2")
+        agent_2.execute(experiment)
         experiment.end()
     else:
         raise Exception("unknown subcommand: " + args.subcommand)
