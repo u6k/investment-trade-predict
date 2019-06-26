929a95b47d4a84208eee0ff0c75315c18e8e762f
u6k <u6k.apps@gmail.com>
2019-06-26 21:52:14 +0900

    Changed: calculate index and simulate trade in preprocess

--- investment_stocks_predict_trend/preprocess.py
+++ investment_stocks_predict_trend/preprocess.py
@@ -5,6 +5,7 @@ import os
 
 def preprocess():
     input_base_path = "local/stock_prices"
+    input_2_base_path = "local/simulate_trade_2"
     output_base_path = "local/stock_prices_preprocessed"
 
     os.makedirs(output_base_path, exist_ok=True)
@@ -17,58 +18,69 @@ def preprocess():
     for ticker_symbol in df_companies.index:
         print(f"ticker_symbol: {ticker_symbol}")
 
-        df_prices = pd.read_csv(f"{input_base_path}/stock_prices.{ticker_symbol}.csv", index_col=0) \
-            .sort_values("date") \
-            .drop_duplicates()
-        df_prices["id"] = np.arange(len(df_prices))
-        df_prices = df_prices.set_index("id")
+        try:
+            df_prices = pd.read_csv(f"{input_base_path}/stock_prices.{ticker_symbol}.csv", index_col=0) \
+                .sort_values("date") \
+                .drop_duplicates()
+            df_prices["id"] = np.arange(len(df_prices))
+            df_prices = df_prices.set_index("id")
 
-        # Simple Moving Average
-        for sma_len in [5, 10, 20, 40, 80]:
-            df_prices[f"sma_{sma_len}"] = df_prices["adjusted_close_price"].rolling(sma_len).mean()
+            # Profit rate
+            df_simulated = pd.read_csv(f"{input_2_base_path}/result.{ticker_symbol}.csv", index_col=0)
 
-        # Momentum
-        for momentum_len in [5, 10, 20, 40, 80]:
-            df_prices[f"momentum_{momentum_len}"] = df_prices["adjusted_close_price"] - df_prices["adjusted_close_price"].shift(momentum_len-1)
+            df_prices["end_id"] = df_simulated["end_id"]
+            df_prices["sell_price"] = df_simulated["sell_price"]
+            df_prices["profit"] = df_simulated["profit"]
+            df_prices["profit_rate"] = df_simulated["profit_rate"]
 
-        # ROC (Rate Of Change)
-        for roc_len in [5, 10, 20, 40, 80]:
-            df_prices[f"roc_{roc_len}"] = df_prices["adjusted_close_price"].pct_change(roc_len-1)
+            # Simple Moving Average
+            for sma_len in [5, 10, 20, 40, 80]:
+                df_prices[f"sma_{sma_len}"] = df_prices["adjusted_close_price"].rolling(sma_len).mean()
 
-        # RSI
-        for rsi_len in [5, 10, 14, 20, 40]:
-            diff = df_prices["adjusted_close_price"].diff()
-            diff = diff[1:]
-            up, down = diff.copy(), diff.copy()
-            up[up < 0] = 0
-            down[down > 0] = 0
-            up_sma = up.rolling(window=rsi_len, center=False).mean()
-            down_sma = down.rolling(window=rsi_len, center=False).mean()
-            rsi = up_sma / (up_sma - down_sma) * 100.0
+            # Momentum
+            for momentum_len in [5, 10, 20, 40, 80]:
+                df_prices[f"momentum_{momentum_len}"] = df_prices["adjusted_close_price"] - df_prices["adjusted_close_price"].shift(momentum_len-1)
 
-            df_prices[f"rsi_{rsi_len}"] = rsi
+            # ROC (Rate Of Change)
+            for roc_len in [5, 10, 20, 40, 80]:
+                df_prices[f"roc_{roc_len}"] = df_prices["adjusted_close_price"].pct_change(roc_len-1)
 
-        # Stochastic
-        for stochastic_len in [5, 9, 20, 25, 40]:
-            close = df_prices["close_price"]
-            low = df_prices["low_price"]
-            low_min = low.rolling(window=stochastic_len, center=False).min()
-            high = df_prices["high_price"]
-            high_max = high.rolling(window=stochastic_len, center=False).max()
+            # RSI
+            for rsi_len in [5, 10, 14, 20, 40]:
+                diff = df_prices["adjusted_close_price"].diff()
+                diff = diff[1:]
+                up, down = diff.copy(), diff.copy()
+                up[up < 0] = 0
+                down[down > 0] = 0
+                up_sma = up.rolling(window=rsi_len, center=False).mean()
+                down_sma = down.rolling(window=rsi_len, center=False).mean()
+                rsi = up_sma / (up_sma - down_sma) * 100.0
 
-            stochastic_k = ((close - low_min) / (high_max - low_min)) * 100
-            stochastic_d = stochastic_k.rolling(window=3, center=False).mean()
-            stochastic_sd = stochastic_d.rolling(window=3, center=False).mean()
+                df_prices[f"rsi_{rsi_len}"] = rsi
 
-            df_prices[f"stochastic_k_{stochastic_len}"] = stochastic_k
-            df_prices[f"stochastic_d_{stochastic_len}"] = stochastic_d
-            df_prices[f"stochastic_sd_{stochastic_len}"] = stochastic_sd
+            # Stochastic
+            for stochastic_len in [5, 9, 20, 25, 40]:
+                close = df_prices["close_price"]
+                low = df_prices["low_price"]
+                low_min = low.rolling(window=stochastic_len, center=False).min()
+                high = df_prices["high_price"]
+                high_max = high.rolling(window=stochastic_len, center=False).max()
 
-        # Summary
-        df_companies.at[ticker_symbol, "data_size"] = len(df_prices)
-        for year in range(2008, 2019):
-            df_companies.at[ticker_symbol, f"volume_{year}"] = df_prices.query(f"'{year}-01-01' <= date <= '{year}-12-31'")["volume"].sum()
+                stochastic_k = ((close - low_min) / (high_max - low_min)) * 100
+                stochastic_d = stochastic_k.rolling(window=3, center=False).mean()
+                stochastic_sd = stochastic_d.rolling(window=3, center=False).mean()
 
-        # Save
-        df_prices.to_csv(f"{output_base_path}/stock_prices.{ticker_symbol}.csv")
-        df_companies.to_csv(f"{output_base_path}/companies.csv")
+                df_prices[f"stochastic_k_{stochastic_len}"] = stochastic_k
+                df_prices[f"stochastic_d_{stochastic_len}"] = stochastic_d
+                df_prices[f"stochastic_sd_{stochastic_len}"] = stochastic_sd
+
+            # Summary
+            df_companies.at[ticker_symbol, "data_size"] = len(df_prices)
+            for year in range(2008, 2019):
+                df_companies.at[ticker_symbol, f"volume_{year}"] = df_prices.query(f"'{year}-01-01' <= date <= '{year}-12-31'")["volume"].sum()
+
+            # Save
+            df_prices.to_csv(f"{output_base_path}/stock_prices.{ticker_symbol}.csv")
+            df_companies.to_csv(f"{output_base_path}/companies.csv")
+        except Exception as err:
+            print(err)
