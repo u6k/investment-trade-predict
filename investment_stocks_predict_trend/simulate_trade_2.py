a2341310947926ac2865ce2729cd3c25c9ea243a
u6k <u6k.apps@gmail.com>
2019-06-27 17:39:20 +0900

    Changed: simulate trade 2

--- investment_stocks_predict_trend/simulate_trade_2.py
+++ investment_stocks_predict_trend/simulate_trade_2.py
@@ -1,43 +1,57 @@
 import pandas as pd
+import numpy as np
 
 
 def execute():
-    input_base_path = "local/stock_prices_preprocessed"
+    input_base_path = "local/stock_prices"
     output_base_path = "local/simulate_trade_2"
 
-    df_companies = pd.read_csv(f"{input_base_path}/companies.csv", index_col=0)
+    df_companies = pd.read_csv(f"{input_base_path}/companies.csv", index_col=0) \
+        .sort_values("ticker_symbol") \
+        .drop_duplicates() \
+        .set_index("ticker_symbol")
+
     for ticker_symbol in df_companies.index:
         print(ticker_symbol)
 
         df_prices = pd.read_csv(f"{input_base_path}/stock_prices.{ticker_symbol}.csv", index_col=0)
+        df_prices = df_prices.sort_values("date")
+        df_prices = df_prices.drop_duplicates()
+        df_prices["id"] = np.arange(len(df_prices))
+        df_prices = df_prices.set_index("id")
+
         df_result = simulate_trade(df_prices)
 
-        df_result.to_csv(f"{output_base_path}/result.{ticker_symbol}.csv")
+        df_result.to_csv(f"{output_base_path}/simulate_trade_2.{ticker_symbol}.csv")
 
 
-def simulate_trade(df_prices_preprocessed, losscut_rate=0.95):
-    df = df_prices_preprocessed.copy()
+def simulate_trade(df_prices, losscut_rate=0.95):
+    df = df_prices.copy()
 
     # simulate
     for start_id in df.index:
-        losscut_price = df.at[start_id, "close_price"] * losscut_rate
+        # reset
+        losscut_price = df.at[start_id, "open_price"] * losscut_rate
         end_id = None
         current_id = start_id + 1
 
         while df.index[-1] > current_id:
-            if df.at[current_id, "close_price"] < losscut_price:
+            # losscut
+            if df.at[current_id, "open_price"] < losscut_price:
                 end_id = current_id + 1
                 break
 
-            if losscut_price < (df.at[current_id, "close_price"] * losscut_rate):
-                losscut_price = df.at[current_id, "close_price"] * losscut_rate
+            # update losscut price
+            if losscut_price < (df.at[current_id, "open_price"] * losscut_rate):
+                losscut_price = df.at[current_id, "open_price"] * losscut_rate
 
             current_id += 1
 
+        # set result
         if end_id is not None:
             df.at[start_id, "end_id"] = end_id
             df.at[start_id, "sell_price"] = df.at[end_id, "open_price"]
-            df.at[start_id, "profit"] = df.at[end_id, "open_price"] - df.at[start_id, "close_price"]
-            df.at[start_id, "profit_rate"] = df.at[end_id, "open_price"] / df.at[start_id, "close_price"]
+            df.at[start_id, "profit"] = df.at[end_id, "open_price"] - df.at[start_id, "open_price"]
+            df.at[start_id, "profit_rate"] = df.at[end_id, "open_price"] / df.at[start_id, "open_price"]
 
     return df
