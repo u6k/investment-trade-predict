a4a64355e22e6dfbee9f25a160e56d3d6c01d3d3
u6k <u6k.apps@gmail.com>
2019-07-18 17:44:23 +0900

    Added: backtest all in simulate_trade_5

--- investment_stocks_predict_trend/simulate_trade_5.py
+++ investment_stocks_predict_trend/simulate_trade_5.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+import pandas as pd
+
 from app_logging import get_app_logger
 import app_s3
 from simulate_trade_base import SimulateTradeBase
@@ -80,6 +83,154 @@ class SimulateTrade5(SimulateTradeBase):
             result["exception"] = err
 
         return result
+
+    def backtest_all(self, s3_bucket, base_path):
+        L = get_app_logger()
+        L.info("start")
+
+        start_date = datetime(2018, 1, 1)
+        end_date = datetime(2019, 1, 1)
+
+        df_action = pd.DataFrame(columns=["date", "ticker_symbol", "action", "price", "stocks", "profit", "profit_rate"])
+        df_stocks = pd.DataFrame(columns=["buy_price", "buy_stocks", "open_price_latest"])
+        df_result = pd.DataFrame(columns=["fund", "asset"])
+
+        df_report = app_s3.read_dataframe(s3_bucket, f"{base_path}/report.csv", index_col=0)
+
+        df_prices_dict = {}
+        for ticker_symbol in df_report.query("expected_value>0.01 and trade_count>30").sort_values("expected_value", ascending=False).index:
+            L.info(f"load data: {ticker_symbol}")
+            df_prices_dict[ticker_symbol] = app_s3.read_dataframe(s3_bucket, f"{base_path}/stock_prices.{ticker_symbol}.csv", index_col=0)
+
+        fund = 100000
+        asset = fund
+        available_rate = 0.1
+        total_available_rate = 0.5
+        fee_rate = 0.001
+        tax_rate = 0.21
+
+        for date in self.date_range(start_date, end_date):
+            date_str = date.strftime("%Y-%m-%d")
+            L.info(f"backtest_all: {date_str}")
+
+            # Sell
+            for ticker_symbol in df_stocks.index:
+                df_prices = df_prices_dict[ticker_symbol]
+
+                if len(df_prices.query(f"date=='{date_str}'")) == 0:
+                    continue
+
+                sell_price = df_prices.query(f"date=='{date_str}'")["open_price"].values[0]
+
+                buy_price = df_stocks.at[ticker_symbol, "buy_price"]
+                buy_stocks = df_stocks.at[ticker_symbol, "buy_stocks"]
+
+                profit = (sell_price - buy_price) * buy_stocks
+                profit_rate = profit / (sell_price * buy_stocks)
+
+                fund += sell_price * buy_stocks
+
+                action_id = len(df_action)
+                df_action.at[action_id, "date"] = date_str
+                df_action.at[action_id, "ticker_symbol"] = ticker_symbol
+                df_action.at[action_id, "action"] = "sell"
+                df_action.at[action_id, "price"] = sell_price
+                df_action.at[action_id, "stocks"] = buy_stocks
+                df_action.at[action_id, "profit"] = profit
+                df_action.at[action_id, "profit_rate"] = profit_rate
+
+                fee_price = (sell_price * buy_stocks) * fee_rate
+                fund -= fee_price
+
+                action_id = len(df_action)
+                df_action.at[action_id, "date"] = date_str
+                df_action.at[action_id, "ticker_symbol"] = ticker_symbol
+                df_action.at[action_id, "action"] = "fee"
+                df_action.at[action_id, "price"] = fee_price
+                df_action.at[action_id, "stocks"] = 1
+
+                if profit > 0:
+                    tax_price = profit * tax_rate
+                    fund -= tax_price
+
+                    action_id = len(df_action)
+                    df_action.at[action_id, "date"] = date_str
+                    df_action.at[action_id, "ticker_symbol"] = ticker_symbol
+                    df_action.at[action_id, "action"] = "tax"
+                    df_action.at[action_id, "price"] = tax_price
+                    df_action.at[action_id, "stocks"] = 1
+
+                df_stocks = df_stocks.drop(ticker_symbol)
+
+            # Buy
+            for ticker_symbol in df_prices_dict.keys():
+                df_prices = df_prices_dict[ticker_symbol]
+
+                if len(df_prices.query(f"date=='{date_str}'")) == 0:
+                    continue
+
+                prices_id = df_prices.query(f"date=='{date_str}'").index[0]
+
+                if df_prices.at[prices_id, "action"] != "buy":
+                    continue
+
+                buy_price = df_prices.at[prices_id, "open_price"]
+                buy_stocks = asset * total_available_rate * available_rate // buy_price
+
+                if buy_stocks == 0:
+                    continue
+
+                fund -= buy_price * buy_stocks
+
+                action_id = len(df_action)
+                df_action.at[action_id, "date"] = date_str
+                df_action.at[action_id, "ticker_symbol"] = ticker_symbol
+                df_action.at[action_id, "action"] = "buy"
+                df_action.at[action_id, "price"] = buy_price
+                df_action.at[action_id, "stocks"] = buy_stocks
+
+                fee_price = (buy_price * buy_stocks) * fee_rate
+                fund -= fee_price
+
+                action_id = len(df_action)
+                df_action.at[action_id, "date"] = date_str
+                df_action.at[action_id, "ticker_symbol"] = ticker_symbol
+                df_action.at[action_id, "action"] = "fee"
+                df_action.at[action_id, "price"] = fee_price
+                df_action.at[action_id, "stocks"] = 1
+
+                df_stocks.at[ticker_symbol, "buy_price"] = buy_price
+                df_stocks.at[ticker_symbol, "buy_stocks"] = buy_stocks
+                df_stocks.at[ticker_symbol, "open_price_latest"] = buy_price
+
+            # Turn end
+            for ticker_symbol in df_stocks.index:
+                df_prices = df_prices_dict[ticker_symbol]
+
+                if len(df_prices.query(f"date=='{date_str}'")) == 0:
+                    continue
+
+                prices_id = df_prices.query(f"date=='{date_str}'").index[0]
+
+                df_stocks.at[ticker_symbol, "open_price_latest"] = df_prices.at[prices_id, "open_price"]
+
+            asset = fund
+            for ticker_symbol in df_stocks.index:
+                asset += df_stocks.at[ticker_symbol, "open_price_latest"] * df_stocks.at[ticker_symbol, "buy_stocks"]
+
+            df_result.at[date_str, "fund"] = fund
+            df_result.at[date_str, "asset"] = asset
+
+            L.info(df_result.loc[date_str])
+
+        app_s3.write_dataframe(df_action, s3_bucket, f"{base_path}/backtest_all.action.csv")
+        app_s3.write_dataframe(df_result, s3_bucket, f"{base_path}/backtest_all.result.csv")
+
+        L.info("finish")
+
+    def date_range(self, start, end):
+        for n in range((end - start).days):
+            yield start + timedelta(n)
 
 
 if __name__ == "__main__":
