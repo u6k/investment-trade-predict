e1fce593853231d962e5a5db432d8fdd9fe9bacd
u6k <u6k.apps@gmail.com>
2019-07-18 15:19:42 +0900

    Added: preprocess 6, simulate trade 5, and changed preprocess, simulate trade

--- investment_stocks_predict_trend/simulate_trade_5.py
+++ investment_stocks_predict_trend/simulate_trade_5.py
@@ -3,10 +3,10 @@ import app_s3
 from simulate_trade_base import SimulateTradeBase
 
 
-class SimulateTrade3(SimulateTradeBase):
+class SimulateTrade5(SimulateTradeBase):
     def simulate_singles_impl(self, ticker_symbol, s3_bucket, input_base_path, output_base_path):
         L = get_app_logger(ticker_symbol)
-        L.info(f"simulate_trade_3: {ticker_symbol}")
+        L.info(f"simulate_trade_5: {ticker_symbol}")
 
         result = {
             "ticker_symbol": ticker_symbol,
@@ -16,8 +16,10 @@ class SimulateTrade3(SimulateTradeBase):
         try:
             df = app_s3.read_dataframe(s3_bucket, f"{input_base_path}/stock_prices.{ticker_symbol}.csv", index_col=0)
 
-            df["day_trade_profit_rate"] = df["close_price"] / df["open_price"]
-            df["day_trade_profit_flag"] = df["day_trade_profit_rate"].apply(lambda r: 1 if r > 1.0 else 0)
+            df["buy_price"] = df["open_price"].shift(-1)
+            df["sell_price"] = df["open_price"].shift(-2)
+            df["profit"] = df["sell_price"] - df["buy_price"]
+            df["profit_rate"] = df["profit"] / df["sell_price"]
 
             app_s3.write_dataframe(df, s3_bucket, f"{output_base_path}/stock_prices.{ticker_symbol}.csv")
         except Exception as err:
@@ -28,7 +30,7 @@ class SimulateTrade3(SimulateTradeBase):
 
     def backtest_singles_impl(self, ticker_symbol, start_date, end_date, s3_bucket, input_prices_base_path, input_preprocess_base_path, input_model_base_path, output_base_path):
         L = get_app_logger(f"backtest_singles.{ticker_symbol}")
-        L.info(f"backtest_singles_3: {ticker_symbol}")
+        L.info(f"backtest_singles_5: {ticker_symbol}")
 
         result = {
             "ticker_symbol": ticker_symbol,
@@ -36,6 +38,8 @@ class SimulateTrade3(SimulateTradeBase):
         }
 
         try:
+            buy_price = None
+
             # Load data
             clf = app_s3.read_sklearn_model(s3_bucket, f"{input_model_base_path}/model.{ticker_symbol}.joblib")
             df_prices = app_s3.read_dataframe(s3_bucket, f"{input_prices_base_path}/stock_prices.{ticker_symbol}.csv", index_col=0)
@@ -50,18 +54,25 @@ class SimulateTrade3(SimulateTradeBase):
 
             # Backtest
             for id in target_period_ids:
-                # Trade
-                if df_prices.at[id-1, "predict"] == 1:
-                    buy_price = df_prices.at[id, "open_price"]
-                    sell_price = df_prices.at[id, "close_price"]
+                # Sell
+                if buy_price is not None:
+                    sell_price = df_prices.at[id, "open_price"]
                     profit = sell_price - buy_price
                     profit_rate = profit / sell_price
 
-                    df_prices.at[id, "action"] = "trade"
+                    df_prices.at[id, "action"] = "sell"
                     df_prices.at[id, "buy_price"] = buy_price
                     df_prices.at[id, "sell_price"] = sell_price
                     df_prices.at[id, "profit"] = profit
                     df_prices.at[id, "profit_rate"] = profit_rate
+
+                    buy_price = None
+
+                # Buy
+                if buy_price is None and df_prices.at[id-1, "predict"] == 1:
+                    buy_price = df_prices.at[id, "open_price"]
+
+                    df_prices.at[id, "action"] = "buy"
 
             app_s3.write_dataframe(df_prices, s3_bucket, f"{output_base_path}/stock_prices.{ticker_symbol}.csv")
         except Exception as err:
@@ -73,10 +84,10 @@ class SimulateTrade3(SimulateTradeBase):
 
 if __name__ == "__main__":
     s3_bucket = "u6k"
-    input_base_path = "ml-data/stocks/preprocess_1.test"
-    output_base_path = "ml-data/stocks/simulate_trade_3.test"
+    input_base_path = "ml-data/stocks/preprocess_1.20190717"
+    output_base_path = "ml-data/stocks/simulate_trade_5.20190717"
 
-    SimulateTrade3().simulate_singles(
+    SimulateTrade5().simulate_singles(
         s3_bucket,
         input_base_path,
         output_base_path
