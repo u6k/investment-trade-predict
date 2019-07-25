65d16d263807afd0006d67038297628d36fb606a
u6k <u6k.apps@gmail.com>
2019-07-25 14:18:28 +0900

    Changed: refactor preprocess 2

--- investment_stocks_predict_trend/preprocess_2.py
+++ investment_stocks_predict_trend/preprocess_2.py
@@ -1,19 +1,17 @@
 import argparse
 import joblib
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from app_logging import get_app_logger
 import app_s3
 
 
-def execute(*, s3_bucket, input_base_path, output_base_path):
-    L = get_app_logger()
+def execute(s3_bucket, input_base_path, output_base_path):
+    L = get_app_logger("preprocess_2")
     L.info("start")
 
     df_companies = app_s3.read_dataframe(s3_bucket, f"{input_base_path}/companies.csv", index_col=0)
-    df_companies_result = pd.DataFrame(columns=df_companies.columns)
+    df_result = pd.DataFrame(columns=df_companies.columns)
 
     results = joblib.Parallel(n_jobs=-1)([joblib.delayed(preprocess)(ticker_symbol, s3_bucket, input_base_path, output_base_path) for ticker_symbol in df_companies.index])
 
@@ -22,9 +20,9 @@ def execute(*, s3_bucket, input_base_path, output_base_path):
             continue
 
         ticker_symbol = result["ticker_symbol"]
-        df_companies_result.loc[ticker_symbol] = df_companies.loc[ticker_symbol]
+        df_result.loc[ticker_symbol] = df_companies.loc[ticker_symbol]
 
-    app_s3.write_dataframe(df_companies_result, s3_bucket, f"{output_base_path}/companies.csv")
+    app_s3.write_dataframe(df_result, s3_bucket, f"{output_base_path}/companies.csv")
 
     L.info("finish")
 
@@ -44,95 +42,23 @@ def preprocess(ticker_symbol, s3_bucket, input_base_path, output_base_path):
         # Volume change rate
         df["volume_change"] = df["volume"] / df["volume"].shift(1)
 
-        # Standardize volume change rate
-        df["volume_change_std"] = StandardScaler().fit_transform(df["volume_change"].values.reshape(-1, 1))
-
-        # MinMax volume change rate
-        df["volume_change_minmax"] = MinMaxScaler().fit_transform(df["volume_change"].values.reshape(-1, 1))
-
         # Adjusted close price change rate
         df["adjusted_close_price_change"] = df["adjusted_close_price"] / df["adjusted_close_price"].shift(1)
-
-        # Standardize adjusted close price change rate
-        df["adjusted_close_price_change_std"] = StandardScaler().fit_transform(df["adjusted_close_price_change"].values.reshape(-1, 1))
-
-        # MinMax adjusted close price change rate
-        df["adjusted_close_price_change_minmax"] = MinMaxScaler().fit_transform(df["adjusted_close_price_change"].values.reshape(-1, 1))
 
         # SMA (Simple Moving Average)
         sma_len_array = [5, 10, 20, 40, 80]
         for sma_len in sma_len_array:
             df[f"sma_{sma_len}"] = df["adjusted_close_price"].rolling(sma_len).mean()
 
-        # Standardize SMA
-        sma = []
-        for sma_len in sma_len_array:
-            sma = np.append(sma, df[f"sma_{sma_len}"].values)
-
-        scaler = StandardScaler().fit(sma.reshape(-1, 1))
-
-        for sma_len in sma_len_array:
-            df[f"sma_{sma_len}_std"] = scaler.transform(df[f"sma_{sma_len}"].values.reshape(-1, 1))
-
-        # MinMax SMA
-        sma = []
-        for sma_len in sma_len_array:
-            sma = np.append(sma, df[f"sma_{sma_len}"].values)
-
-        scaler = MinMaxScaler().fit(sma.reshape(-1, 1))
-
-        for sma_len in sma_len_array:
-            df[f"sma_{sma_len}_minmax"] = scaler.transform(df[f"sma_{sma_len}"].values.reshape(-1, 1))
-
         # Momentum
         momentum_len_array = [5, 10, 20, 40, 80]
         for momentum_len in momentum_len_array:
             df[f"momentum_{momentum_len}"] = df["adjusted_close_price"] - df["adjusted_close_price"].shift(momentum_len-1)
 
-        # Standardize momentum
-        momentum = []
-        for momentum_len in momentum_len_array:
-            momentum = np.append(momentum, df[f"momentum_{momentum_len}"].values)
-
-        scaler = StandardScaler().fit(momentum.reshape(-1, 1))
-
-        for momentum_len in momentum_len_array:
-            df[f"momentum_{momentum_len}_std"] = scaler.transform(df[f"momentum_{momentum_len}"].values.reshape(-1, 1))
-
-        # MinMax momentum
-        momentum = []
-        for momentum_len in momentum_len_array:
-            momentum = np.append(momentum, df[f"momentum_{momentum_len}"].values)
-
-        scaler = MinMaxScaler().fit(momentum.reshape(-1, 1))
-
-        for momentum_len in momentum_len_array:
-            df[f"momentum_{momentum_len}_minmax"] = scaler.transform(df[f"momentum_{momentum_len}"].values.reshape(-1, 1))
-
         # ROC (Rate Of Change)
         roc_len_array = [5, 10, 20, 40, 80]
         for roc_len in roc_len_array:
             df[f"roc_{roc_len}"] = df["adjusted_close_price"].pct_change(roc_len-1)
-
-        # Standardize ROC
-        roc = []
-        for roc_len in roc_len_array:
-            roc = np.append(roc, df[f"roc_{roc_len}"].values)
-
-        scaler = StandardScaler().fit(roc.reshape(-1, 1))
-
-        for roc_len in roc_len_array:
-            df[f"roc_{roc_len}_std"] = scaler.transform(df[f"roc_{roc_len}"].values.reshape(-1, 1))
-
-        # MinMax ROC
-        roc = []
-        for roc_len in roc_len_array:
-            roc = np.append(roc, df[f"roc_{roc_len}"].values)
-
-        scaler = MinMaxScaler().fit(roc.reshape(-1, 1))
-
-        for roc_len in roc_len_array:
-            df[f"roc_{roc_len}_minmax"] = scaler.transform(df[f"roc_{roc_len}"].values.reshape(-1, 1))
 
         # RSI
         rsi_len_array = [5, 10, 14, 20, 40]
@@ -147,26 +73,6 @@ def preprocess(ticker_symbol, s3_bucket, input_base_path, output_base_path):
             rsi = up_sma / (up_sma - down_sma) * 100.0
 
             df[f"rsi_{rsi_len}"] = rsi
-
-        # Standardize RSI
-        rsi = []
-        for rsi_len in rsi_len_array:
-            rsi = np.append(rsi, df[f"rsi_{rsi_len}"].values)
-
-        scaler = StandardScaler().fit(rsi.reshape(-1, 1))
-
-        for rsi_len in rsi_len_array:
-            df[f"rsi_{rsi_len}_std"] = scaler.transform(df[f"rsi_{rsi_len}"].values.reshape(-1, 1))
-
-        # MinMax RSI
-        rsi = []
-        for rsi_len in rsi_len_array:
-            rsi = np.append(rsi, df[f"rsi_{rsi_len}"].values)
-
-        scaler = MinMaxScaler().fit(rsi.reshape(-1, 1))
-
-        for rsi_len in rsi_len_array:
-            df[f"rsi_{rsi_len}_minmax"] = scaler.transform(df[f"rsi_{rsi_len}"].values.reshape(-1, 1))
 
         # Stochastic
         stochastic_len_array = [5, 9, 20, 25, 40]
@@ -185,33 +91,8 @@ def preprocess(ticker_symbol, s3_bucket, input_base_path, output_base_path):
             df[f"stochastic_d_{stochastic_len}"] = stochastic_d
             df[f"stochastic_sd_{stochastic_len}"] = stochastic_sd
 
-        # Standardize Stochastic
-        stochastic = []
-        for stochastic_len in stochastic_len_array:
-            stochastic = np.append(stochastic, df[f"stochastic_k_{stochastic_len}"].values)
-            stochastic = np.append(stochastic, df[f"stochastic_d_{stochastic_len}"].values)
-            stochastic = np.append(stochastic, df[f"stochastic_sd_{stochastic_len}"].values)
-
-        scaler = StandardScaler().fit(stochastic.reshape(-1, 1))
-
-        for stochastic_len in stochastic_len_array:
-            df[f"stochastic_k_{stochastic_len}_std"] = scaler.transform(df[f"stochastic_k_{stochastic_len}"].values.reshape(-1, 1))
-            df[f"stochastic_d_{stochastic_len}_std"] = scaler.transform(df[f"stochastic_d_{stochastic_len}"].values.reshape(-1, 1))
-            df[f"stochastic_sd_{stochastic_len}_std"] = scaler.transform(df[f"stochastic_sd_{stochastic_len}"].values.reshape(-1, 1))
-
-        # MinMax Stochastic
-        stochastic = []
-        for stochastic_len in stochastic_len_array:
-            stochastic = np.append(stochastic, df[f"stochastic_k_{stochastic_len}"].values)
-            stochastic = np.append(stochastic, df[f"stochastic_d_{stochastic_len}"].values)
-            stochastic = np.append(stochastic, df[f"stochastic_sd_{stochastic_len}"].values)
-
-        scaler = MinMaxScaler().fit(stochastic.reshape(-1, 1))
-
-        for stochastic_len in stochastic_len_array:
-            df[f"stochastic_k_{stochastic_len}_minmax"] = scaler.transform(df[f"stochastic_k_{stochastic_len}"].values.reshape(-1, 1))
-            df[f"stochastic_d_{stochastic_len}_minmax"] = scaler.transform(df[f"stochastic_d_{stochastic_len}"].values.reshape(-1, 1))
-            df[f"stochastic_sd_{stochastic_len}_minmax"] = scaler.transform(df[f"stochastic_sd_{stochastic_len}"].values.reshape(-1, 1))
+        # Drop columns
+        df = df.drop(["ticker_symbol", "open_price", "high_price", "low_price", "close_price", "volume", "adjusted_close_price"], axis=1)
 
         # Save
         app_s3.write_dataframe(df, s3_bucket, f"{output_base_path}/stock_prices.{ticker_symbol}.csv")
