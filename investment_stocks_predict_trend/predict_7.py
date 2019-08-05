9b845f08854c041ab062ad2dc8b0496229293fdc
u6k <u6k.apps@gmail.com>
2019-08-05 14:03:22 +0900

    Added: be sure to buy in predict 7

--- investment_stocks_predict_trend/predict_7.py
+++ investment_stocks_predict_trend/predict_7.py
@@ -8,6 +8,6 @@ class PredictClassification_7(PredictClassificationBase):
         pass
 
     def model_predict(self, ticker_symbol, df_data):
-        df_data["predict"] = np.random.randint(0, 2, (len(df_data), ))
+        df_data["predict"] = np.ones(len(df_data))
 
         return df_data
