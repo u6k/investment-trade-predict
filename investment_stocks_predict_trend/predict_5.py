8d4be6dfe70bac4bfdb3189e598b559db72b3329
u6k <u6k.apps@gmail.com>
2019-08-07 16:21:41 +0900

    Changed: SVM predictor

--- investment_stocks_predict_trend/predict_5.py
+++ investment_stocks_predict_trend/predict_5.py
@@ -1,20 +1,43 @@
 import argparse
 
 from sklearn.svm import SVC
+# from sklearn import model_selection
 from predict_base import PredictClassificationBase
 import app_s3
 
 
 class PredictClassification_5(PredictClassificationBase):
     def model_fit(self, x_train, y_train):
-        return SVC(gamma="scale").fit(x_train, y_train)
+        return SVC(C=100.0, kernel="rbf", gamma="scale", random_state=0).fit(x_train, y_train)
+        # params = {
+        #    "C": [0.1, 1.0, 10.0, 100.0],
+        #    "kernel": ["rbf"],
+        #    "gamma": ["scale"],
+        #    "random_state": [0]
+        # }
+
+        # clf = model_selection.GridSearchCV(
+        #    SVC(),
+        #    params,
+        #    cv=5,
+        #    n_jobs=-1,
+        #    verbose=1
+        # )
+
+        # clf.fit(x_train, y_train)
+
+        # print(f"best_params: {clf.best_params_}")
+
+        # clf_best = clf.best_estimator_
+
+        # return clf_best
 
     def model_predict(self, ticker_symbol, df_data):
         model = app_s3.read_sklearn_model(self._s3_bucket, f"{self._output_base_path}/model.{ticker_symbol}.joblib")
 
-        pred = model.predict(df_data.values)
+        df_data["predict"] = model.predict(df_data.values)
 
-        return pred
+        return df_data
 
 
 if __name__ == "__main__":
